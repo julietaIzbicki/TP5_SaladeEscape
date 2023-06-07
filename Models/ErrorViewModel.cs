1eaea91d2c78fbfabaabe7e2b492fe346e964b62
MAIA KUPERSMID <48045053@ort>
2023-06-07 09:32:32 -0300

    creando

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP5_SaladeEscape.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
